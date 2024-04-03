08b0e105df7aabe635e717e8381d8e7d889f5936
ErvinCaravali <73136209+ErvinCaravali@users.noreply.github.com>
2024-04-03 14:15:50 -0500

    se añade sombras , metricas y ambiente (#8)

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,25 +1,22 @@
-import { OrbitControls } from "@react-three/drei";
-import { useFrame } from '@react-three/fiber';
-import { useRef } from 'react';
-import  World  from './world/World.jsx'
-import  Box  from './box/Box.jsx';
-import  Cone from './cone/Cone.jsx'
-import  Sphere from './sphere/Sphere.jsx'
-import './styles.css';
-const Experience = () => {
-    const object1 = useRef(null);
+import { BakeShadows, Loader, OrbitControls } from "@react-three/drei";
+import World from "./world/World";
+import Lights from "./lights/Lights";
+import Environments from "./environments/Environments";
+import { Perf } from "r3f-perf";
+import { Suspense } from "react";
 
- 
-    
+const Experience = () => {
     return (
         <>
-            <ambientLight />
-            <directionalLight position={[10, 10, 5]} />
-            <OrbitControls />
-            <World/>
-            <Box/>
-            <Cone/>
-            <Sphere/>
+            <Perf position="top-left" />
+            <BakeShadows />
+            <OrbitControls makeDefault />
+
+            <Suspense fallback={null}>
+                <Lights />
+                <Environments />
+                <World />
+            </Suspense>
         </>
     )
 }
