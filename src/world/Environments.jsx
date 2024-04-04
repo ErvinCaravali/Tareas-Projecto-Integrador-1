5e11d402526f6725d39eef61ef3fe6c7923b3205
ErvinCaravali <ervin.caravali@correounivalle.edu.co>
2024-04-04 13:42:03 -0500

    se reactualiza todo

--- src/world/Environments.jsx
+++ src/world/Environments.jsx
@@ -3,7 +3,7 @@ import { Environment} from "@react-three/drei";
 export default function Environments() {
     return <>
         <Environment
-            files={"https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/umhlanga_sunrise_4k.hdr"}
+            files={"https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/8k/harvest_8k.hdr"}
             preset={null}
             background={false}
             ground={{
--- src/world/World.jsx
+++ src/world/World.jsx
@@ -7,7 +7,7 @@ export default function World(props) {
     return (
         <group {...props} dispose={null}>
             <group>
-                <mesh onClick={(e)=>e.stopPropagation()} geometry={nodes.Walls.geometry} material={materials.wallMaterial} />
+                {<mesh onClick={(e)=>e.stopPropagation()} geometry={nodes.Walls.geometry} material={materials.wallMaterial} /> }
                 <mesh onClick={(e)=>e.stopPropagation()} receiveShadow={true} geometry={nodes.Floor.geometry} material={materials.floorMaterial} />
                 <mesh onClick={(e)=>e.stopPropagation()} castShadow={true} geometry={nodes.WoodenFence.geometry} material={materials.woodMaterial} />
                 <mesh onClick={(e)=>e.stopPropagation()} geometry={nodes.Tree.geometry} material={materials.treeMaterial} />
